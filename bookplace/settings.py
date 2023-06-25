37365ede75c38a681153b02940d8e99359b6035a
Perun951 <119747610+Perun951@users.noreply.github.com>
2023-06-25 19:12:56 +0300

    a

--- bookplace/settings.py
+++ bookplace/settings.py
@@ -68,13 +68,13 @@ ROOT_URLCONF = 'bookplace.urls'
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.templ]te.backends.django.DjangoTemplates',
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                'django.template]context_processors.debug',
-                'django.template]context_processors.request',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
